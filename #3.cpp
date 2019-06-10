63e46d17c15d8dea6859ac9a99f709d7846866ac
Altkove <49477360+Altkove@users.noreply.github.com>
2019-06-10 14:03:25 +0300

    Create #3.cpp

---
+++ #3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+int main()
+{
+ string s;
+ getline(cin,s);
+ s.length();
+ for (int x = 0; x < s.length(); x++)
+ {
+ if(s[x]=='O')
+            s[x]=char(s[x]+32);
+     if(s[x]=='E')
+            s[x]=char(s[x]+32);
+     if(s[x]=='I')
+            s[x]=char(s[x]+32);
+     if(s[x]=='A')
+            s[x]=char(s[x]+32);
+if(s[x]=='U')
+            s[x]=char(s[x]+32);
+     if(s[x]=='Y')
+            s[x]=char(s[x]+32);
+ } 
+     cout << s << endl;
+ return 0;
+}
+
