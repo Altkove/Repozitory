9e5f6c3fea92c0accc115e71216e2d083b609280
Altkove <49477360+Altkove@users.noreply.github.com>
2019-06-22 15:01:26 +0300

    Update #2.cpp

--- #2.cpp
+++ #2.cpp
@@ -9,55 +9,43 @@ int z()
 {
     for (int x = 0; x < s.length(); x++)
     {
-        {
             if (s[x] == 'o')
                 d++;
-        } 
         while (s[x] == 'o')
         {
             s.erase(x,1);
             x--;
         }
-        {
             if (s[x] == 'e')
                 f++;
-        }
         while (s[x] == 'e')
         {
             s.erase(x,1);
             x--;
         }
-        {
             if (s[x] == 'i')
                 g++;
-        }
         while (s[x] == 'i')
         {
             s.erase(x,1);
             x--;
         }
-        {
             if (s[x] == 'a')
                 h++;
-        }
         while (s[x] == 'a')
         {
             s.erase(x,1);
             x--;
         }
-        {
             if (s[x] == 'u')
                 j++;
-        }
         while (s[x] == 'u')
         {
             s.erase(x,1);
             x--;
         }
-        {
             if (s[x] == 'y')
                 k++;
-        }
         while (s[x] == 'y')
         {
             s.erase(x,1);
